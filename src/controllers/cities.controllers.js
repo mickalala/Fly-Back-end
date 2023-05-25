59818e83babe536ece5c1cb04bc025b19b6ebe23
mickalala <mickalarissa01@gmail.com>
2023-05-25 19:58:05 -0300

    get city by id

--- src/controllers/cities.controllers.js
+++ src/controllers/cities.controllers.js
@@ -2,7 +2,7 @@ import { db } from "../databaseConnection/database.connection.js";
 
 export async function getCities(req, res) {
     try {
-        const { rows: allCities } = db.query(`SELECT * FROM cities;`)
+        const { rows: allCities } = await db.query(`SELECT * FROM cities;`)
 
         res.status(200).send(allCities);
 
@@ -13,9 +13,10 @@ export async function getCities(req, res) {
 
 export async function getCityById(req, res) {
     const { id } = req.params
-    try{
-
-    }catch(err){
+    try {
+        const { rows: city } = await db.query(`SELECT * FROM cities WHERE id=$1;`, [id])
+        res.status(200).send(city)
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
--- src/routers/cities.routers.js
+++ src/routers/cities.routers.js
@@ -4,5 +4,6 @@ import { getCities,getCityById } from "../controllers/cities.controllers.js";
 const citiesRouter=Router();
 
 citiesRouter.get("/cities", getCities)
+citiesRouter.get("/cities/:id",getCityById)
 
 export default citiesRouter;
