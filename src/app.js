bcd58a5ee2584574c449a6272a08ef99d4bdc4e4
mickalala <mickalarissa01@gmail.com>
2023-05-29 17:46:33 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ import indexRouter from "./routers/index.routes.js"
 
 dotenv.config()
 
-const app= express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(indexRouter)
 
 
-const PORT=5000;
-
-app.listen(PORT, ()=>console.log(`tá rodando na portaaa::: ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
